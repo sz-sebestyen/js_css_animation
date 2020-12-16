a8ae4f4349cfdda84fbc8064a2daa437b55c08fd
sz-sebestyen <szabosebestyen.mail@gmail.com>
2020-12-16 09:59:10 +0100

    Write content from array

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ function _load() {
 		{
 			tag: "h1",
 			content: "Szabó Sebestyén",
-			
+
+		},
+		{
+			tag: "p",
+			content: "Lorem ipsum",
 		},
 	];
+/* 
+	for (let index = 0; index < array.length; index++) {
+		const element = array[index];
+		
+	} */
+
+	const root = document.getElementById("root");
+
+	for (const iterator of arr) {
+
+		root.insertAdjacentHTML("beforeend", `
+			<${iterator.tag}>${iterator.content}</${iterator.tag}>
+		`);
+
+		console.log(iterator.tag);
+	}
+
+	
 
 }
 
